e96fe17dd95059a59d7b1dbba7849b28ab8e5fdf
yachu7 <yachumaharjan@gmail.com>
2024-01-17 15:40:54 +0545

    added login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ const Users = require("../models/Users");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const User = require("../models/Users");
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
 const JWT_SECRET = "HelloMyNameiSBillI";
 
@@ -26,12 +26,12 @@ router.post(
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
-        return res.status(
-          (400)).json({ error: "Sorry user with this email already exists" }
-        );
+        return res
+          .status(400)
+          .json({ error: "Sorry user with this email already exists" });
       }
       const salt = await bcrypt.genSalt(10);
-       const secpass = await bcrypt.hash(req.body.password,salt );
+      const secpass = await bcrypt.hash(req.body.password, salt);
 
       // create user
       user = await Users.create({
@@ -39,19 +39,60 @@ router.post(
         email: req.body.email,
         password: secpass,
       });
-      const data ={
-        user : {
-          id : user.id
-        }
-      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
       const authToken = jwt.sign(data, JWT_SECRET);
-      
-      res.json({authToken});
+
+      res.json({ authToken });
     } catch (error) {
-      console.error(error.message)
-      res.status(500).send("some error occured")
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
+// Login using POST : (/api/auth/login) doesnt require auth
+router.post(
+  "/login",
+  [
+    body("email", "Enter Valid Email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are error return bad req and error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({email});
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
